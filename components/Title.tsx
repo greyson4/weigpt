aae0047d8d8da7e3c57f0f8dd6bef48a7041a7e0
greyson4 <129144827+greyson4@users.noreply.github.com>
2023-03-28 17:34:13 +0800

    Update Title.tsx

--- components/Title.tsx
+++ components/Title.tsx
@@ -8,7 +8,7 @@ export const Title: FC = () => {
         className={`w-inherit fixed z-10 top-0 py-3.5 bg-gray-wx text-center text-lg border-b-[0.5px] border-gray-300
                    md:-mx-4 md:pt-[1.375rem]`}
       >
-        ChatGPT Next
+        Wei ChatGPT
       </h1>
     </>
   );
